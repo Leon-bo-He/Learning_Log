18baf6c56caaab61e875f67cbe2b2ef806835183
Bo <bohe.ch2@gmail.com>
2023-01-13 16:13:16 -0500

    Modified to a user-friendly style

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
 
+    # third party apps
+    'bootstrap4',
+
     # load admin after users
     'django.contrib.admin',
     'django.contrib.auth',
